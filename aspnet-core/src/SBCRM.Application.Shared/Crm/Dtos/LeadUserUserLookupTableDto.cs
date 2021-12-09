2faaf6bc692d4b19e00bfab188c331a1803a8ba3
Andrés Jácome <jjacome@webcreek.com>
2021-12-09 15:36:15 -0500

    Added LeadUser table with Power Tools

---
+++ aspnet-core/src/SBCRM.Application.Shared/Crm/Dtos/LeadUserUserLookupTableDto.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace SBCRM.Crm.Dtos
+{
+    public class LeadUserUserLookupTableDto
+    {
+        public long Id { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
---
+++ aspnet-core/src/SBCRM.Core/Crm/LeadUser.cs
@@ -0,0 +1,26 @@
+﻿using SBCRM.Crm;
+using SBCRM.Authorization.Users;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Entities;
+
+namespace SBCRM.Crm
+{
+    [Table("LeadUsers")]
+    public class LeadUser : FullAuditedEntity
+    {
+
+        public virtual int? LeadId { get; set; }
+
+        [ForeignKey("LeadId")]
+        public Lead LeadFk { get; set; }
+
+        public virtual long? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User UserFk { get; set; }
+
+    }
+}
