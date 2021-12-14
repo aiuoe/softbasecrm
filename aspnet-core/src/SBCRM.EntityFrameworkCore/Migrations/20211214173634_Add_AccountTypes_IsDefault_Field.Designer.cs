b47c3252751da8b0c05e8c2c874e0184ac777705
Jose Poma <jpoma@webcreek.com>
2021-12-14 13:32:26 -0500

    OS20BCS-56 - Add column by IsDefault in AccountTypes

--- aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20211214173634_Add_AccountTypes_IsDefault_Field.Designer.cs
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20211214173634_Add_AccountTypes_IsDefault_Field.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SBCRM.EntityFrameworkCore;
 
 namespace SBCRM.Migrations
 {
     [DbContext(typeof(SBCRMDbContext))]
-    partial class SBCRMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211214173634_Add_AccountTyes_IsDefault_Field")]
+    partial class Add_AccountTypes_IsDefault_Field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20211214173634_Add_AccountTypes_IsDefault_Field.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBCRM.Migrations
+{
+    public partial class Add_AccountTypes_IsDefault_Field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                schema: "web",
+                table: "AccountTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                schema: "web",
+                table: "AccountTypes");
+        }
+    }
+}
