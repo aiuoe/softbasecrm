cb6860758afab89470a505a26a9fb2864bea6883
CarlosCerVel <ccerda@webcreek.com>
2021-12-15 14:39:15 -0500

    - database-setup.sql

--- scripts/database-setup.sql
+++ scripts/database-setup.sql
@@ -6,6 +6,16 @@ VALUES ('New', getdate(), '#2C4AB6', 0, 1),
        ('Dead', getdate(), '#263950', 0, 1)
 GO
 
+INSERT INTO [web].[OpportunityStages] ([Description], [CreationTime], [Color], [IsDeleted])
+VALUES ('Needs Analysis', getdate(), '#2C4AB6', 0),
+	   ('Qualification', getdate(), '#2C4AB6', 0),
+	   ('Prospecting', getdate(), '#2C4AB6', 0),
+       ('Proposal', getdate(), '#FF8900', 0),
+	   ('Negotiation', getdate(), '#FF8900', 0),
+       ('Closed / Won', getdate(), '#008E26', 0),
+       ('Closed / Lost', getdate(), '#FF1414', 0)
+GO
+
 INSERT INTO [web].[AccountTypes] ([Description], [CreationTime], [IsDeleted], IsDefault)
 VALUES ('Open', getdate(), 0, 0),
        ('Prospect', getdate(), 0, 1)
