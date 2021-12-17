f20c7f392fb20b0f0047df4d990bf9117cc0653c
Hiram Acebo <hacebo@webcreek.com>
2021-12-17 15:36:07 -0600

    Create script for initial migration from legacy users to web.AbpUsers

---
+++ scripts/user-import.sql
@@ -0,0 +1,29 @@
+USE SBCRMDb
+GO
+
+BEGIN TRY
+	BEGIN TRANSACTION
+    DECLARE @InsertedEmployeeNo table(EmployeeNo int);
+
+	INSERT INTO [web].[AbpUsers] ([UserName],[NormalizedUserName],[Name],[Surname], [EmailAddress],[NormalizedEmailAddress], [PhoneNumber], [Password],[CreationTime],[IsActive],[AccessFailedCount],[IsEmailConfirmed],[IsTwoFactorEnabled],[IsLockoutEnabled],[IsPhoneNumberConfirmed],[ShouldChangePasswordOnNextLogin], [IsDeleted])
+		OUTPUT [inserted].[UserName]
+			INTO @InsertedEmployeeNo
+	SELECT TOP 1 [S].[EmployeeNo],[S].[EmployeeNo], [P].[FirstName], ISNULL([P].[LastName],'') AS LastName, ISNULL([P].[EMailAddress],''),ISNULL([P].[EMailAddress],''), P.Phone, S.[Password], GETDATE(),1,0,0,0,0,0,0,0
+	FROM [dbo].[secure] AS s
+		JOIN dbo.Person AS p
+			ON s.EmployeeNo = p.Number
+
+	SELECT EmployeeNo AS 'Imported Employees' FROM @InsertedEmployeeNo
+
+    COMMIT TRANSACTION
+
+  END TRY
+  BEGIN CATCH
+	DECLARE @Message varchar(MAX) = ERROR_MESSAGE(),
+			@Severity int = ERROR_SEVERITY(),
+			@State smallint = ERROR_STATE()
+	PRINT(@Message)
+-- Transaction uncommittable
+    IF (XACT_STATE()) = -1
+      ROLLBACK TRANSACTION
+  END CATCH
