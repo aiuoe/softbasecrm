7537af3ee0c93271ba4573881bc06929b2e65883
phuriev <phuriev@webcreek.com>
2021-12-24 22:00:45 +0200

    OS20BCS-85 - Account global search
    Global search of all accounts

--- angular/src/app/main/crm/globalSearch/global-search.component.ts
+++ angular/src/app/main/crm/globalSearch/global-search.component.ts
@@ -1,11 +1,23 @@
-import { Component, Injector, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, Injector, ViewEncapsulation, ViewChild, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+    CustomerServiceProxy,
+    AccountUserDto,
+    GetCustomerForViewDto,
+    AccountUserUserLookupTableDto,
+    CustomerAccountTypeLookupTableDto
+} from '@shared/service-proxies/service-proxies';
+import { NotifyService } from 'abp-ng2-module';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { TokenAuthServiceProxy } from '@shared/service-proxies/service-proxies';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
-import { LazyLoadEvent } from '@node_modules/primeng/api';
-import { Table } from '@node_modules/primeng/table';
-import { Paginator } from '@node_modules/primeng/paginator';
-import { ActivatedRoute } from '@angular/router';
-import { DataService } from '@app/shared/common/crm/services/data.service';
+import { Table } from 'primeng/table';
+import { Paginator } from 'primeng/paginator';
+import { LazyLoadEvent } from 'primeng/api';
+import { FileDownloadService } from '@shared/utils/file-download.service';
+import { DateTimeService } from '@app/shared/common/timing/date-time.service';
+import { AppConsts } from '@shared/AppConsts';
+import { LocalStorageService } from '@shared/utils/local-storage.service';
 
 @Component({
     templateUrl: './global-search.component.html',
@@ -17,41 +29,61 @@ export class GlobalSearchComponent extends AppComponentBase implements OnInit {
     @ViewChild('dataTable', { static: true }) dataTable: Table;
     @ViewChild('paginator', { static: true }) paginator: Paginator;
 
-    results: any[];
-    textFilter: string;
+    advancedFiltersAreShown = false;
+    textFilter = '';
+    accountTypes: CustomerAccountTypeLookupTableDto[];
+    selectedAccountType: CustomerAccountTypeLookupTableDto;
+    selectedAccountTypes: CustomerAccountTypeLookupTableDto[];
+    accountUsers: AccountUserDto[] = [];
+    assignedUsersFilter: AccountUserUserLookupTableDto[] = [];
 
     constructor(
         injector: Injector,
+        private _customerServiceProxy: CustomerServiceProxy,
+        private _notifyService: NotifyService,
+        private _tokenAuth: TokenAuthServiceProxy,
         private _activatedRoute: ActivatedRoute,
-        private _dataService: DataService
+        private _fileDownloadService: FileDownloadService,
+        private _router: Router,
+        private _dateTimeService: DateTimeService,
+        private _localStorageService: LocalStorageService
     ) {
         super(injector);
+        this._router.routeReuseStrategy.shouldReuseRoute = function () {
+            return false;
+        }
     }
 
     ngOnInit(): void {
-        this.textFilter = this._activatedRoute.snapshot.queryParams['filter'];
-        this.results = [];
-        this.results.push({ name: 'ABC', module: 'Account' });
-        this.results.push({ name: 'L CDE', module: 'Lead' });
+        this.textFilter=this._activatedRoute.snapshot.queryParams['filter'];
 
-        this._dataService.globalSearch
-            .subscribe(x => {
-               if (x) {
-                   this.textFilter = x;
-                   this.getGlobalSearch();
-               }
+        this._customerServiceProxy.getAllAccountTypeForTableDropdown()
+            .subscribe((result: CustomerAccountTypeLookupTableDto[]) => {
+                this.accountTypes = result;
             });
     }
 
     getGlobalSearch(event?: LazyLoadEvent) {
-        if (this.textFilter) {
-            if (this.primengTableHelper.shouldResetPaging(event)) {
-                this.paginator.changePage(0);
-                return;
-            }
-
-            this.primengTableHelper.totalRecordsCount = this.results.length;
-            this.primengTableHelper.records = this.results;
+        if (this.primengTableHelper.shouldResetPaging(event)) {
+            this.paginator.changePage(0);
+            return;
         }
+
+        this.primengTableHelper.showLoadingIndicator();
+
+        this._customerServiceProxy
+            .getAll(
+                this.textFilter,
+                this.selectedAccountTypes?.map(x => x.id),
+                this.assignedUsersFilter?.map(x => x.id),
+                this.primengTableHelper.getSorting(this.dataTable),
+                this.primengTableHelper.getSkipCount(this.paginator, event),
+                this.primengTableHelper.getMaxResultCount(this.paginator, event)
+            )
+            .subscribe((result) => {
+                this.primengTableHelper.totalRecordsCount = result.totalCount;
+                this.primengTableHelper.records = result.items;
+                this.primengTableHelper.hideLoadingIndicator();
+            });
     }
 }
