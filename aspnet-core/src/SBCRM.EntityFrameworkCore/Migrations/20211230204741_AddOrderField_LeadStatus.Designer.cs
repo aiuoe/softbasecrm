505c544194313ed99338a559428080ecb8ff2803
Jesus Garcia <jgarcia@webcreek.com>
2021-12-30 17:16:38 -0500

    OS20BCS-67
    Configuration / Lead Status
    https://webcreek.atlassian.net/browse/OS20BCS-67

--- aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20211230204741_AddOrderField_LeadStatus.Designer.cs
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20211230204741_AddOrderField_LeadStatus.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SBCRM.EntityFrameworkCore;
 
 namespace SBCRM.Migrations
 {
     [DbContext(typeof(SBCRMDbContext))]
-    partial class SBCRMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211230204741_AddOrderField_LeadStatus")]
+    partial class AddOrderField_LeadStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
