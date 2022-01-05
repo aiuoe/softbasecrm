b21e718ce58da37366eec293192e1845107eaeec
MManlod <mmanlod@webcreek.com>
2022-01-06 06:20:27 +0800

    Added customer attachment upload

--- angular/src/app/main/crm/customerAttachments/create-or-edit-customerAttachment-modal.component.ts
+++ angular/src/app/main/crm/customerAttachments/create-or-edit-customerAttachment-modal.component.ts
@@ -1,5 +1,8 @@
-﻿import { Component, ViewChild, Injector, Output, EventEmitter, OnInit, ElementRef } from '@angular/core';
+﻿import { TokenService } from 'abp-ng2-module';
+import { Component, ViewChild, Injector, Output, EventEmitter, OnInit, ElementRef } from '@angular/core';
+import { AppConsts } from '@shared/AppConsts';
 import { ModalDirective } from 'ngx-bootstrap/modal';
+import { FileUploader, FileUploaderOptions, FileItem } from 'ng2-file-upload';
 import { finalize } from 'rxjs/operators';
 import {
     CustomerAttachmentsServiceProxy,
@@ -16,16 +19,21 @@ import { DateTimeService } from '@app/shared/common/timing/date-time.service';
 })
 export class CreateOrEditCustomerAttachmentModalComponent extends AppComponentBase implements OnInit {
     @ViewChild('createOrEditModal', { static: true }) modal: ModalDirective;
+    @ViewChild('uploadFileInputLabel') uploadFileInputLabel: ElementRef;
 
     @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
 
     active = false;
     saving = false;
+    public uploader: FileUploader;
+
+    private _uploaderOptions: FileUploaderOptions = {};
 
     customerAttachment: CreateOrEditCustomerAttachmentDto = new CreateOrEditCustomerAttachmentDto();
 
     constructor(
         injector: Injector,
+        private _tokenService: TokenService,
         private _customerAttachmentsServiceProxy: CustomerAttachmentsServiceProxy,
         private _dateTimeService: DateTimeService
     ) {
@@ -38,6 +46,7 @@ export class CreateOrEditCustomerAttachmentModalComponent extends AppComponentBa
             this.customerAttachment.id = customerAttachmentId;
 
             this.active = true;
+            this.initFileUploader();
             this.modal.show();
         } else {
             this._customerAttachmentsServiceProxy
@@ -46,31 +55,78 @@ export class CreateOrEditCustomerAttachmentModalComponent extends AppComponentBa
                     this.customerAttachment = result.customerAttachment;
 
                     this.active = true;
+                    this.initFileUploader();
                     this.modal.show();
                 });
         }
     }
 
+    /**
+     * This method manages the upload of the excel file
+     */
+     initFileUploader(): void {
+        this.uploader = new FileUploader({url: AppConsts.remoteServiceBaseUrl + '/CustomerImport/UploadAttachments'});
+        this._uploaderOptions.autoUpload = false;
+        this._uploaderOptions.authToken = 'Bearer ' + this._tokenService.getToken();
+        this._uploaderOptions.removeAfterUpload = true;
+        this.uploader.onAfterAddingFile = (file) => {
+            file.withCredentials = false;
+        };
+
+        this.uploader.onBuildItemForm = (fileItem: FileItem, form: any) => {
+            form.append('Id', this.customerAttachment.id);
+            form.append('Name', this.customerAttachment.name);
+            form.append('FilePath', this.customerAttachment.filePath);
+        };
+
+        this.uploader.onSuccessItem = (item, response, status) => {
+            this.message.success(this.l('CustomerAttachmentUploaded'));
+            this.close();
+        };
+
+        this.uploader.onErrorItem = () => {
+            //this.message.error(this.l('ErrorUploadingMessage'));
+            this.close();
+        };
+
+        this.uploader.setOptions(this._uploaderOptions);
+
+        this.uploader.response.subscribe(res => {
+            this.modalSave.emit(null);
+        });
+    }
+
     save(): void {
         this.saving = true;
 
-        this._customerAttachmentsServiceProxy
-            .createOrEdit(this.customerAttachment)
-            .pipe(
-                finalize(() => {
-                    this.saving = false;
-                })
-            )
-            .subscribe(() => {
-                this.notify.info(this.l('SavedSuccessfully'));
-                this.close();
-                this.modalSave.emit(null);
-            });
+        this.message.confirm('', this.l('AreYouSureToUpload'), (isConfirmed) => {
+            if (isConfirmed) {
+                this.uploader.uploadAll();
+                this.saving = false;
+            }
+        });
     }
 
     close(): void {
         this.active = false;
         this.modal.hide();
+    }
+
+    fileChangeEvent(event: any): void {
+        const validExtensions = ['jpg', 'jpeg', 'png', 'gif', 'doc', 'pdf'];
+        const selectedFile = event.target.files[0];
+        const filename: string = selectedFile.name;
+        const fileExtension: string = filename.substring(filename.lastIndexOf('.') + 1, filename.length) || filename;
+
+        if (!validExtensions.some((ext) => ext.toLowerCase() === fileExtension.toLowerCase())) {
+            // Invalid file extension
+            this.message.warn(this.l('CustomerAttachment_Warn_FileType'));
+            return;
+        }
+
+        this.uploadFileInputLabel.nativeElement.innerText = filename;
+        this.uploader.clearQueue();
+        this.uploader.addToQueue([event.target.files[0]]);
     }
 
     ngOnInit(): void {}
---
+++ aspnet-core/src/SBCRM.Web.Host/Controllers/CustomerImportController.cs
@@ -0,0 +1,86 @@
+﻿using SBCRM.Crm;
+using SBCRM.Dto;
+using SBCRM.Web.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.UI;
+using SBCRM.Authorization.Users.Profile;
+using SBCRM.Authorization.Users.Profile.Dto;
+using SBCRM.Storage;
+using System;
+using Microsoft.Extensions.Hosting;
+using SBCRM.Crm.Dtos;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Abp.AspNetCore.Mvc.Authorization;
+
+namespace SBCRM.Web.Controllers
+{
+    /// <summary>
+    /// Customers Controller to manage specific methods as endpoints
+    /// </summary>
+    [AbpMvcAuthorize]
+    public class CustomerImportController : SBCRMControllerBase
+    {
+        private ICustomerAttachmentsAppService _customerAttachmentAppService { get; set; }
+        private readonly IHostEnvironment _env;
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="customerAttachmentAppService"></param>
+        public CustomerImportController(
+            ICustomerAttachmentsAppService customerAttachmentAppService,
+            IHostEnvironment env)
+        {
+            _customerAttachmentAppService = customerAttachmentAppService;
+            _env = env;
+        }
+
+        /// <summary>
+        /// This method recieves an input file as a customer attachment.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<int?> UploadAttachments(CustomerAttachmentDto input)
+        {
+            var attachment = Request.Form.Files.First();
+            var extension = Path.GetExtension(attachment.FileName);
+
+            try
+            {
+                if (!(string.Equals(extension, ".jpg", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(extension, ".png", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(extension, ".gif", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(extension, ".jpeg", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(extension, ".doc", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(extension, ".pdf", StringComparison.OrdinalIgnoreCase)))
+                {
+                    throw new UserFriendlyException(L("ErrorUploadingMessage"));
+                }
+
+                var dir = Path.Combine(_env.ContentRootPath, "Attachments");
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                var dto = new CreateOrEditCustomerAttachmentDto
+                {
+                    Name = input.Name,
+                    FilePath = Path.Combine(dir, attachment.FileName)
+                };
+
+                await attachment.CopyToAsync(new FileStream(dto.FilePath, FileMode.Create));
+                await _customerAttachmentAppService.CreateOrEdit(dto);
+
+                return dto.Id;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                throw new UserFriendlyException(L("ErrorUploadingMessage"));
+            }
+        }
+    }
+}
