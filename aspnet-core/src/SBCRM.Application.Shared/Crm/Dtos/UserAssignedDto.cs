6516be98dfe93ee5b386bc318a791feeb7f6471e
CarlosCerVel <ccerda@webcreek.com>
2022-01-07 12:52:44 +1300

    OS20BCS-669 - moved leadquerydto to the correct folder

---
+++ aspnet-core/src/SBCRM.Application.Shared/Crm/Dtos/UserAssignedDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SBCRM.Crm.Dtos
+{
+    public class UserAssignedDto
+    {
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FullName { get; set; }
+
+        public Guid? ProfilePictureId { get; set; }
+    }
+}
