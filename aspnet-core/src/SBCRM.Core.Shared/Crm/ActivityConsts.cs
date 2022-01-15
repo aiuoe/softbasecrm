dcaa1fdbaebfdfcf5315152edc2ac70e33799555
Kevin Cabanilla <96188025+webcreek-kcabanilla@users.noreply.github.com>
2022-01-15 09:08:40 +0800

    Activity Export - Excluded the time if the activity is a reminder-type

--- aspnet-core/src/SBCRM.Core.Shared/Crm/ActivityConsts.cs
+++ aspnet-core/src/SBCRM.Core.Shared/Crm/ActivityConsts.cs
@@ -6,5 +6,10 @@
         public const int MinTaskNameLength = 1;
         public const int MaxTaskNameLength = 100;
 
+        public const string SCHEDULE_MEETING = "SCHEDULE_MEETING";
+        public const string SCHEDULE_CALL = "SCHEDULE_CALL";
+        public const string EMAIL_REMINDER = "EMAIL_REMINDER";
+        public const string TODO_REMINDER = "TODO_REMINDER";
+
     }
 }
