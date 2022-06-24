8720058985ce4ab5601d7a3e8f0f56350bd8ccb8
Jose Poma <jpoma@webcreek.com>
2022-06-24 11:42:18 -0500

    DN20CC-26 - New backend approach using "use case" services
    - Delegate the authorization responsibility to controllers
    - Use api versioning and restful apis as much as possible

---
+++ aspnet-core/src/SBCRM.Application/Modules/Administration/Branch/Handlers/CreateBranchCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SBCRM.Base;
+using SBCRM.Modules.Administration.Branch.Commands;
+using SBCRM.Modules.Administration.Dtos;
+
+namespace SBCRM.Modules.Administration.Branch.Handlers
+{
+    /// <summary>
+    /// Create branch use case command handler
+    /// </summary>
+    public class CreateBranchCommandHandler : SBCRMAppServiceBase, IRequestHandler<CreateBranchCommand, GetBranchForEditDto>
+    {
+        private readonly IBranchRepository _branchRepository;
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="branchRepository"></param>
+        public CreateBranchCommandHandler(IBranchRepository branchRepository)
+        {
+            _branchRepository = branchRepository;
+        }
+
+        /// <summary>
+        /// Create branch use case
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<GetBranchForEditDto> Handle(CreateBranchCommand command, CancellationToken cancellationToken)
+        {
+            var branch = new Core.BaseEntities.Branch
+            {
+                Name = command.Name,
+                Number = command.Number,
+                SubName = command.SubName,
+                Address = command.Address
+            };
+            SetTenant(branch);
+            branch.Id = await _branchRepository.InsertAndGetIdAsync(branch);
+            return new GetBranchForEditDto
+            {
+                AdditionalPropertyA = "A",
+                AdditionalPropertyB = "B",
+                Branch = ObjectMapper.Map<BranchDto>(branch)
+            };
+        }
+    }
+
+}
