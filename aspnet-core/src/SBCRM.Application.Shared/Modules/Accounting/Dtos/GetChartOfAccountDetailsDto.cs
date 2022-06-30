c55ba5cf537668f23aa452b76e67bff6df8ffb1b
t1vr <tanviramd32@gmail.com>
2022-07-01 01:57:07 +0600

    handler,query and dto added

---
+++ aspnet-core/src/SBCRM.Application.Shared/Modules/Accounting/Dtos/GetChartOfAccountDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SBCRM.Modules.Accounting.Dtos
+{
+    /// <summary>
+    /// DTO for fetching ChartOfAccount details
+    /// </summary>
+    public class GetChartOfAccountDetailsDto
+    {
+        public string Description { get; set; }
+    }
+}
