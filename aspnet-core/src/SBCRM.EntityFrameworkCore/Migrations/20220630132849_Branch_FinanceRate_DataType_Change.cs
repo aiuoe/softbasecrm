5012b165d5e2c7dff6e08e1044086e30d9f3a3e8
Iftifar Taz <itaz@wcgclinical.com>
2022-06-30 19:30:19 +0600

    column type update, Camel case fix

---
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220630132849_Branch_FinanceRate_DataType_Change.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBCRM.Migrations
+{
+    public partial class Branch_FinanceRate_DataType_Change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FinanceRate",
+                schema: "web",
+                table: "Branch",
+                type: "decimal(19,4)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FinanceRate",
+                schema: "web",
+                table: "Branch",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(19,4)",
+                oldNullable: true);
+        }
+    }
+}
