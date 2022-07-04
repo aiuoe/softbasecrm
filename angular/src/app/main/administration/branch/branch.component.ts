9cddafeee19f927290070209f2b81e697d552706
Iftifar Taz <itaz@wcgclinical.com>
2022-07-05 02:05:40 +0600

    get data and set values

--- angular/src/app/main/administration/branch/branch.component.ts
+++ angular/src/app/main/administration/branch/branch.component.ts
@@ -1,32 +1,54 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { BreadcrumbItem } from '@app/shared/common/sub-header/sub-header.component';
-import { CustomerCountryLookupTableDto } from '@shared/service-proxies/service-proxies';
+import {
+    BranchesServiceProxy, IAccountReceivableInBranchDto, IBranchLookupDto, ICurrencyTypeLookupDto,
+    ICustomerCountryLookupTableDto, IGetBranchInitialDataDto, ITaxCodeInBranchDto, IWarehouseLookupDto
+} from '@shared/service-proxies/service-proxies';
+import { Subject } from 'rxjs';
 
 @Component({
     templateUrl: './branch.component.html',
     animations: [appModuleAnimation()]
 })
 
-export class BranchComponent extends AppComponentBase {
+export class BranchComponent extends AppComponentBase implements OnInit {
 
-    saving: boolean = false;    
+    destroy$ = new Subject();
+    saving: boolean = false;
     breadcrumbs: BreadcrumbItem[] = [
         new BreadcrumbItem(this.l('Administration')),
         new BreadcrumbItem(this.l('Branch'))
     ];
-    
-    branches: any[];
-    countries: CustomerCountryLookupTableDto[] = [];
-    arAccounts: any[];
-    warehouses: any[];
-    currencies: any[];
+
+    branches: IBranchLookupDto[] = [];
+    countries: ICustomerCountryLookupTableDto[] = [];
+    accountsReceivables: IAccountReceivableInBranchDto[] = [];
+    warehouses: IWarehouseLookupDto[] = [];
+    currencyTypes: ICurrencyTypeLookupDto[] = [];
+    taxCodes: ITaxCodeInBranchDto[] = [];
 
     constructor(
-        injector: Injector
+        injector: Injector,
+        private _branchesService: BranchesServiceProxy,
     ) {
         super(injector);
+    }
+
+    ngOnInit(): void {
+        this._branchesService.getInitialData().pipe(takeUntil(this.destroy$)).subscribe((x: IGetBranchInitialDataDto) => {
+            this.branches = x.branches;
+            this.accountsReceivables = x.accountsReceivables;
+            this.warehouses = x.warehouses;
+            this.currencyTypes = x.currencyTypes;
+            this.taxCodes = x.taxCodes;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
     }
 
     add() {
@@ -41,7 +63,7 @@ export class BranchComponent extends AppComponentBase {
 
     }
 
-    logoGraphicClear(){
-        
+    logoGraphicClear() {
+
     }
 }
