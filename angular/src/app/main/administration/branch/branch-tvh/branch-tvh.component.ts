faa4e0bf24dd2226337cb8b1f22a1906b2423077
Iftifar Taz <itaz@wcgclinical.com>
2022-07-08 01:16:17 +0600

    ui unpdate for id/value convertion for dropdown

--- angular/src/app/main/administration/branch/branch-tvh/branch-tvh.component.ts
+++ angular/src/app/main/administration/branch/branch-tvh/branch-tvh.component.ts
@@ -8,30 +8,10 @@ import { BranchForEditDto } from '@shared/service-proxies/service-proxies';
 })
 
 export class BranchTvhComponent extends AppComponentBase {
-    
+
     @Input() branchForEdit: BranchForEditDto;
-
-    countries: any[] = [
-        { displayName: "United States", value: "0" },
-        { displayName: "Canada", value: "1" },
-        { displayName: "Mexico", value: "2" },
-    ];
-
-    warehouses:any[]=[
-        { displayName: "All, US", value: "0" },
-        { displayName: "Kansas, US", value: "1" },
-        { displayName: "California, US", value: "2" },
-        { displayName: "Oregon, US", value: "3" },
-        { displayName: "South Carolina, US", value: "4" },
-        { displayName: "Pennsylvania, US", value: "5" },
-        { displayName: "Illinois, US", value: "6" },
-        { displayName: "Lousiana, US", value: "7" },
-        { displayName: "Florida, US", value: "8" },
-        { displayName: "Ontario, Canada", value: "9" },
-        { displayName: "British Columbia, Canada", value: "10" },
-        { displayName: "Mexico City, Mexico", value: "11" },
-        { displayName: "Monterrey, Mexico", value: "12" },
-    ]
+    @Input() countries: BranchForEditDto;
+    @Input() tvhWarehouses: BranchForEditDto;
 
     constructor(
         injector: Injector
