d43c57c428fa5c98883aa7e21a4bea677cf097da
Iftifar Taz <itaz@wcgclinical.com>
2022-07-08 02:53:28 +0600

    update branch api modification

--- aspnet-core/src/SBCRM.Application/Modules/Administration/Branch/Handlers/UpdateBranchCommandHandler.cs
+++ aspnet-core/src/SBCRM.Application/Modules/Administration/Branch/Handlers/UpdateBranchCommandHandler.cs
@@ -13,14 +13,39 @@ namespace SBCRM.Modules.Administration.Branch.Handlers
     public class UpdateBranchCommandHandler : SBCRMAppServiceBase, IRequestHandler<UpdateBranchCommand, BranchForEditDto>
     {
         private readonly IBranchRepository _branchRepository;
+        private readonly IWarehouseRepository _warehouseRepository;
+        private readonly ITaxRepository _taxRepository;
+        private readonly IStateTaxCodeRepository _stateTaxCodeRepository;
+        private readonly ILocalTaxCodeRepository _localTaxCodeRepository;
+        private readonly ICityTaxCodeRepository _cityTaxCodeRepository;
+        private readonly ICountyTaxCodeRepository _countyTaxCodeRepository;
 
         /// <summary>
         /// Base constructor
         /// </summary>
         /// <param name="branchRepository"></param>
-        public UpdateBranchCommandHandler(IBranchRepository branchRepository)
+        /// <param name="warehouseRepository"></param>
+        /// <param name="taxRepository"></param>
+        /// <param name="stateTaxCodeRepository"></param>
+        /// <param name="localTaxCodeRepository"></param>
+        /// <param name="cityTaxCodeRepository"></param>
+        /// <param name="countyTaxCodeRepository"></param>
+        public UpdateBranchCommandHandler(
+            IBranchRepository branchRepository,
+            IWarehouseRepository warehouseRepository,
+            ITaxRepository taxRepository,
+            IStateTaxCodeRepository stateTaxCodeRepository,
+            ILocalTaxCodeRepository localTaxCodeRepository,
+            ICityTaxCodeRepository cityTaxCodeRepository,
+            ICountyTaxCodeRepository countyTaxCodeRepository)
         {
             _branchRepository = branchRepository;
+            _warehouseRepository = warehouseRepository;
+            _taxRepository = taxRepository;
+            _stateTaxCodeRepository = stateTaxCodeRepository;
+            _localTaxCodeRepository = localTaxCodeRepository;
+            _cityTaxCodeRepository = cityTaxCodeRepository;
+            _countyTaxCodeRepository = countyTaxCodeRepository;
         }
 
         /// <summary>
@@ -32,7 +57,21 @@ namespace SBCRM.Modules.Administration.Branch.Handlers
         public async Task<BranchForEditDto> Handle(UpdateBranchCommand command, CancellationToken cancellationToken)
         {
             var branch = await _branchRepository.GetAsync(command.Id);
+            var warehouse = await _warehouseRepository.FirstOrDefaultAsync(x => x.Id == command.DefaultWarehouseId);
+            var taxCode = await _taxRepository.FirstOrDefaultAsync(x => x.Id == command.TaxCodeId);
+            var stateTaxCode = await _stateTaxCodeRepository.FirstOrDefaultAsync(x => x.Id == command.StateTaxCodeId);
+            var localTaxCode = await _localTaxCodeRepository.FirstOrDefaultAsync(x => x.Id == command.LocalTaxCodeId);
+            var cityTaxCode = await _cityTaxCodeRepository.FirstOrDefaultAsync(x => x.Id == command.CityTaxCodeId);
+            var countyTaxCode = await _countyTaxCodeRepository.FirstOrDefaultAsync(x => x.Id == command.CountyTaxCodeId);
+
             ObjectMapper.Map(command, branch);
+            branch.DefaultWarehouse = warehouse?.WarehouseName;
+            branch.TaxCode = taxCode?.Code;
+            branch.StateTaxCode = stateTaxCode?.TaxCode;
+            branch.LocalTaxCode = localTaxCode?.TaxCode;
+            branch.CityTaxCode = cityTaxCode?.TaxCode;
+            branch.CountyTaxCode = countyTaxCode?.TaxCode;
+
             await _branchRepository.UpdateAsync(branch);
             return ObjectMapper.Map<BranchForEditDto>(branch);
         }
