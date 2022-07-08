5e4ddc978071473e386a074d3ee5305e339985a6
itaz <itaz@webcreek.com>
2022-07-09 00:02:43 +0600

    comment added

--- angular/src/app/main/administration/tax-codes/tax-codes.component.ts
+++ angular/src/app/main/administration/tax-codes/tax-codes.component.ts
@@ -3,12 +3,13 @@ import { AppComponentBase } from '@shared/common/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { BreadcrumbItem } from '@app/shared/common/sub-header/sub-header.component';
 
+/***
+ * Component to manage tax codes
+ */
 @Component({
     templateUrl: './tax-codes.component.html',
     animations: [appModuleAnimation()]
 })
-
-// main Tax Codes component - where tabs are maintained
 export class TaxCodesComponent extends AppComponentBase {
 
     breadcrumbs: BreadcrumbItem[] = [
