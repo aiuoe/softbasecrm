e716bda8ad668d1d08bdcc95fa512e2f80fe151f
Mosab Abdul <mabdul@webcreek.com>
2022-07-08 16:22:43 +0530

    Added Get branch(s) by number API

---
+++ aspnet-core/src/SBCRM.Application/Modules/Common/GetBranches/Commands/GetBranchesByNoCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SBCRM.Modules.Administration.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBCRM.Modules.Common.GetBranches.Commands
+{
+    public class GetBranchesByNoCommand : IRequest<List<GetBranchDto>>
+    {
+        public GetBranchesByNoCommand(short number)
+        {
+            Number = number;
+        }
+
+        public short Number { get; set; }   
+
+    }
+}
