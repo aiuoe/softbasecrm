7b6d27c94d94061785bcf777adfc51056b40bce9
jmanriquewebcreek <jmanrrique@webcreek.com>
2022-07-13 15:01:46 -0400

    add migration

---
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220713185904_changes fields types for ZipCode table.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBCRM.Migrations
+{
+    public partial class changesfieldstypesforZipCodetable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TimeZone",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group6",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group5",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group4",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group3",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group2",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Group1",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Branch",
+                schema: "web",
+                table: "ZipCode",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "TimeZone",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group6",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group5",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group4",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group3",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group2",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Group1",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Branch",
+                schema: "web",
+                table: "ZipCode",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
