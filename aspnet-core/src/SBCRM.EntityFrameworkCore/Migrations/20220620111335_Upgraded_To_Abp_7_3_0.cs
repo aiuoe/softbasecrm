bf7dfbd6c190e8e6a00972d8b0a39cfb89dac5cc
Jose Poma <jpoma@webcreek.com>
2022-07-12 22:10:06 -0500

    Add new entity framework migrations from latest ANZ version

---
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220620111335_Upgraded_To_Abp_7_3_0.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SBCRMDemo.Migrations
+{
+    public partial class Upgraded_To_Abp_7_3_0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TargetNotifiers",
+                schema: "web",
+                table: "AbpUserNotifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TargetNotifiers",
+                schema: "web",
+                table: "AbpNotifications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetNotifiers",
+                schema: "web",
+                table: "AbpUserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "TargetNotifiers",
+                schema: "web",
+                table: "AbpNotifications");
+        }
+    }
+}
