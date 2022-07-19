6669e88b915ec8bd5c457ae7e3204b60b61a99af
itaz <itaz@webcreek.com>
2022-07-19 18:02:19 +0600

    email validation

--- angular/src/app/main/administration/branch/branch-email/branch-email.component.ts
+++ angular/src/app/main/administration/branch/branch-email/branch-email.component.ts
@@ -1,6 +1,8 @@
 import { Component, Injector, Input } from '@angular/core';
+import { AppConsts } from '@shared/AppConsts';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { UpsertBranchDto } from '@shared/service-proxies/service-proxies';
+import { debounce as _debounce } from 'lodash-es';
 
 /**
  * Sub component for branch email tab
@@ -11,9 +13,11 @@ import { UpsertBranchDto } from '@shared/service-proxies/service-proxies';
 })
 
 export class BranchEmailComponent extends AppComponentBase {
-
     @Input() isViewMode: boolean;
     @Input() upsertBranchDto: UpsertBranchDto;
+
+    printFinalCcValid = true;
+    printFinalBccValid = true;
 
     /**
      * constructor
@@ -22,5 +26,31 @@ export class BranchEmailComponent extends AppComponentBase {
         injector: Injector
     ) {
         super(injector);
+    }
+
+    /**
+     * print final cc on input action
+     */
+    printFinalCcOnInput = _debounce(this.setPrintFinalCcValidity, AppConsts.SearchBarDelayMilliseconds);
+
+    /**
+    * print final bcc on input action
+    */
+    printFinalBccOnInput = _debounce(this.setPrintFinalBccValidity, AppConsts.SearchBarDelayMilliseconds);
+
+    /**
+    * set print final cc validity
+    */
+    private setPrintFinalCcValidity(): void {
+        this.printFinalCcValid = this.upsertBranchDto.printFinalCc.split(',').map(x => x.trim())
+            .every(x => x.length !== 0 && x.match(AppConsts.ValidEmailRegex));
+    }
+
+    /**
+    * set print final bcc validity
+    */
+    private setPrintFinalBccValidity(): void {
+        this.printFinalBccValid = this.upsertBranchDto.printFinalBcc.split(',').map(x => x.trim())
+            .every(x => x.length !== 0 && x.match(AppConsts.ValidEmailRegex));
     }
 }
