27453a0a4f2264836d69f4daa06af7dce06907f7
rvillamor-webcreek <rvillamor@webcreek.com>
2022-07-26 02:08:00 +0800

    Fix migration

--- aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220725180518_Fix_Migration.Designer.cs
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220725180518_Fix_Migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SBCRM.EntityFrameworkCore;
 
@@ -11,9 +12,10 @@ using SBCRM.EntityFrameworkCore;
 namespace SBCRM.Migrations
 {
     [DbContext(typeof(SBCRMDbContext))]
-    partial class SBCRMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220725180518_Fix_Migration")]
+    partial class Fix_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/SBCRM.EntityFrameworkCore/Migrations/20220725180518_Fix_Migration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SBCRM.Migrations
+{
+    public partial class Fix_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
